2a091e9b8c8bf6c21e3bd6ad185ca5dd570eb434
darkhacker1906 <singhabhinandan634@gmail.com>
2024-03-23 13:41:51 +0530

    Initial setup

---
+++ todousingvuex/src/App.vue
@@ -0,0 +1,6 @@
+<template>
+<todo-app/>
+</template>
+<script setup>
+import TodoApp from './components/TodoApp.vue'
+</script>
---
+++ todousingvuex/src/components/TodoApp.vue
@@ -0,0 +1,10 @@
+<script>
+export default TodoApp({
+  setup() {
+    
+  },
+})
+</script>
+<template>
+  <h1>Hello this is my hello world page</h1>
+</template>
